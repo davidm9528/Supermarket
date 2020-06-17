2cc8b2d2f690f1f359d97f902c79d64cdf55d1ed
davidm9528 <davidm9528@gmail.com>
2020-06-17 22:21:06 +0100

    Added the Store table and added the Contractor table Minor problems with keys to be fixed

--- schema.sql
+++ schema.sql
@@ -1,9 +1,19 @@
+CREATE TABLE IF NOT EXISTS Store (
+    Store_ID BIGINT AUTO_INCREMENT UNIQUE,
+    Stock_ID BIGINT AUTO_INCREMENT UNIQUE,
+    County CHAR, 
+    PRIMARY KEY(Store_ID)
+    FOREIGN KEY (Stock_ID) REFERENCES Stock(Stock_ID)
+);
+
 CREATE TABLE IF NOT EXISTS Item (
     Item_ID BIGINT AUTO_INCREMENT UNIQUE,
     Provider CHAR,
     Name CHAR,
+    BBD DATE,
     PRIMARY KEY(Item_ID)
 );
+
 CREATE TABLE IF NOT EXISTS Stock (
     Stock_ID BIGINT AUTO_INCREMENT UNIQUE,
     Item_ID BIGINT,
@@ -12,15 +22,18 @@ CREATE TABLE IF NOT EXISTS Stock (
     PRIMARY KEY(Stock_ID),   
     FOREIGN KEY(Item_ID) REFERENCES Item(Item_ID)
 );
+
 CREATE TABLE IF NOT EXISTS Staff (
     Staff_ID BIGINT AUTO_INCREMENT UNIQUE,
+    Store_ID BIGINT UNIQUE,
     F_name CHAR,
     S_name CHAR, 
     DOB DATE,
     Clock_Num BIGINT UNIQUE,
     User_Num BIGINT UNIQUE,
     User_Pass BIGINT, 
-    PRIMARY KEY(Staff_ID)
+    PRIMARY KEY(Staff_ID),
+    FOREIGN KEY(Store_ID) REFERENCES Store(Store_ID)
 );
 
 CREATE TABLE IF NOT EXISTS Contract (
@@ -32,3 +45,14 @@ CREATE TABLE IF NOT EXISTS Contract (
     FOREIGN KEY(Staff_ID) REFERENCES Staff(Staff_ID),
     PRIMARY KEY(Contract_ID)
 );
+
+CREATE TABLE IF NOT EXISTS Contractor (
+    Contractor_ID BIGINT AUTO_INCREMENT UNIQUE,
+    Company CHAR,
+    Store_ID BIGINT UNIQUE,
+    Job CHAR,
+    Job_Start DATE,
+    Job_End DATE,
+    PRIMARY KEY(Contractor_ID),
+    FOREIGN KEY(Store_ID) REFERENCES Store(Store_ID)
+); 
