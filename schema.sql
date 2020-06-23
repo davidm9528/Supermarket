857e73be815b63bd5a6770272913d5ba9b075e46
David Mackenzie <davidm9528@gmail.com>
2020-06-23 23:06:16 +0100

    Changing of FK from AD1 to AD2

--- schema.sql
+++ schema.sql
@@ -10,20 +10,20 @@ CREATE TABLE IF NOT EXISTS Product (
 CREATE TABLE IF NOT EXISTS Addresses (
     AD1 VARCHAR(50),
     AD2 VARCHAR(50),
-    City_Town VARCHAR(20), 
+    City_Town VARCHAR(20),
     District VARCHAR(15),
-    Country VARCHAR(30), 
-    Telephone_No VARCHAR(11) UNIQUE,  
-    PRIMARY KEY(AD1)
+    Country VARCHAR(30),
+    Telephone_No VARCHAR(11) UNIQUE,
+    PRIMARY KEY(AD2)
 );
 
 /*Details about a store*/
 CREATE TABLE IF NOT EXISTS Store (
     Store_ID BIGINT AUTO_INCREMENT UNIQUE,
     Store_Name VARCHAR(30),
-    Address VARCHAR(50), 
+    Address VARCHAR(50),
     PRIMARY KEY(Store_ID),
-    FOREIGN KEY (Address) REFERENCES Addresses(AD1)
+    FOREIGN KEY (Address) REFERENCES Addresses(AD2)
 );
 
 /*Details holding all the details about items in a Store's Stock*/
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS Stock (
     Value INT, 
     Next_Delivery DATE,
     FOREIGN KEY(Product_ID) REFERENCES Product(Product_ID),
-    FOREIGN KEY(Store_ID) REFERENCES Store(Store_ID)  
+    FOREIGN KEY(Store_ID) REFERENCES Store(Store_ID)
 );
 
 /*Details about the staff in different Stores*/
@@ -43,7 +43,7 @@ CREATE TABLE IF NOT EXISTS Staff (
     Staff_ID BIGINT AUTO_INCREMENT UNIQUE,
     Store_ID BIGINT UNIQUE,
     F_name VARCHAR(50),
-    S_name VARCHAR(50), 
+    S_name VARCHAR(50),
     DOB DATE,
     Clock_Num BIGINT UNIQUE,
     User_Num BIGINT UNIQUE,
@@ -57,7 +57,7 @@ CREATE TABLE IF NOT EXISTS Contract (
     Contract_ID BIGINT AUTO_INCREMENT UNIQUE,
     Staff_ID BIGINT,
     Rate float,
-    Weekly_Hrs INT, 
+    Weekly_Hrs INT,
     Holidays_Hrs_Remaining INT,
     FOREIGN KEY(Staff_ID) REFERENCES Staff(Staff_ID),
     PRIMARY KEY(Contract_ID)
